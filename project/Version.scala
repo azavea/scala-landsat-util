89b51fb2ddf253227c9c443fd987c7e956bbfd91
lossyrob <rdemanuele@gmail.com>
2015-12-21 20:22:33 -0500

    Fix to travis config.

--- project/Version.scala
+++ project/Version.scala
@@ -1,9 +1,14 @@
 import scala.util.Properties
 
 object Version {
-  val landsatUtil =
-    "0.1.0" + Properties.envOrElse("LSU_VERSION_SUFFIX", "-SNAPSHOT")
-
+  val landsatUtil = {
+    val tag = Properties.envOrElse("TRAVIS_TAG", "")
+    if(tag == "") {
+      "0.1.0" + Properties.envOrElse("LSU_VERSION_SUFFIX", "-SNAPSHOT")
+    } else {
+      tag
+    }
+  }
 
   val geotrellis  = "0.10.0-9d08114"
   val scala       = "2.10.5"
