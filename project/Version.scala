27a1aca8c842e72526e7a79a8fc32cd68e88d9b3
Colin Woodbury <colingw@gmail.com>
2016-09-12 08:18:29 -0700

    Updated project properties

--- project/Version.scala
+++ project/Version.scala
@@ -4,12 +4,12 @@ object Version {
   val landsatUtil = {
     val tag = Properties.envOrElse("TRAVIS_TAG", "")
     if(tag == "") {
-      "0.2.1" + Properties.envOrElse("LSU_VERSION_SUFFIX", "-SNAPSHOT")
+      "0.3.0" + Properties.envOrElse("LSU_VERSION_SUFFIX", "-SNAPSHOT")
     } else {
       tag
     }
   }
 
-  val geotrellis  = "0.10.0"
+  val geotrellis  = "0.10.2"
   val scala       = "2.10.6"
 }
